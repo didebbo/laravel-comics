fa7c0a0b2c7a0699052e263355bb4549a46ae286
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-05 16:13:24 +0100

    push issues

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    {{-- TODO Header --}} hello world!
+    <div class="top-header">
+        <div class="container">
+            <ul class="row">
+                <li class="col">
+                    DC Power VISA
+                </li>
+                <li class="col">
+                    ADDITIONAL DC SITES
+                </li>
+            </ul>
+        </div>
+    </div>
 </header>
