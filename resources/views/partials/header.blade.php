dc2ee022791a3e49a4a2c30611d58adacddff4ca
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-05 17:27:24 +0100

    .

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,4 +15,63 @@
             </ul>
         </div>
     </div>
+    <div class="container">
+        <nav>
+            <div class="logo">
+                <img src="{{ asset('/images/dc-logo.png') }}" alt="DC Logo">
+            </div>
+            <ul class="row">
+                <li class="col">
+                    <a href="#">
+                        Characters
+                    </a>
+                </li>
+                <li class="col active">
+                    <a href="#">
+                        Comics
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        Movies
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        Games
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        Collectibles
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        Videos
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        Fans
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        News
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#" class="dropdown">
+                        Shop
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
