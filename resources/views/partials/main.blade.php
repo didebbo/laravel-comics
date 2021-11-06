0348ea57549aaea5ffa37491a723ac6687f3978f
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-06 17:06:15 +0100

    completata sezione current series

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,9 +1,11 @@
 <main>
-    {{-- @php
-        dump($data);
-    @endphp --}}
     <section class="current-series">
         <div class="container">
+            <div class="title">
+                <h2>
+                    Current Series
+                </h2>
+            </div>
             <ul class="row">
                 @foreach ($data as $key => $col)
                     <li class="col">
@@ -24,6 +26,11 @@
                     </li>
                 @endforeach
             </ul>
+            <div class="btn-more">
+                <button>
+                    Load More
+                </button>
+            </div>
         </div>
     </section>
 </main>
